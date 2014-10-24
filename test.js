2e77da5085920632daf2393b6dd5a5e540e196ba
nitriques <nico@nitriques.com>
2014-10-24 00:23:24 -0400

    promisify the whole thing up

--- test.js
+++ test.js
@@ -1,27 +1,50 @@
 var Tosr0x = require('./lib/tosr0x').Tosr0x;
 
-Tosr0x.portScan(function (ctl) {
+var ctl;
+
+//Tosr0x.fromPortScan(null, function (err, ctl) {
+Tosr0x.fromPortScan().then(function (c) {
+	ctl = c;
 	console.log('Controller found!');
-	ctl.open(function (ctl) {
-		console.log('Connected!!!');
-		ctl.version(function (version) {
-			console.log('Version is ' + version);
-
-			ctl.refreshStates(function (states) {
-				console.dir(states);
-				ctl.close();
-			});
-
-			/*console.log('Turning realy 1 on');
-			ctl.on(1, function () {
-				console.log('Relay one is on!!!');
-				setTimeout(function () {
-					ctl.off(1, function () {
-						console.log('Relay one is off!!!');
-						ctl.close();
-					});
-				}, 2000);
-			});*/
-		});
+	//ctl.open(function (err, ctl) {
+	return ctl.open();
+})
+.then(function (ctl) {
+	console.log('Connected!!!');
+	return ctl.version();
+})
+.then(function (version) {
+	console.log('Version is ' + version);
+	return ctl.refreshStates();
+})
+.then(function (states) {
+	console.log('State', states);
+})
+.then(function () {
+	console.log('Turning realy 1 on');
+	//ctl.on(1, function () {
+	return ctl.on(0);
+})
+.then(function () {
+	console.log('Relay one is on!!!');
+	return new (require('rsvp').Promise)(function (res, rej) {
+		setTimeout(function () {
+			res();
+		}, 2000);
 	});
+})
+.then(function () {
+	return ctl.off(0);
+})
+.then(function () {
+	return ctl.close();
+})
+.catch(function (err) {
+	console.error('ERROR ' + err);
+})
+.finally(function () {
+	if (!!ctl) {
+		ctl.closeImmediate();
+	}
+	console.log('Exit.');
 });
